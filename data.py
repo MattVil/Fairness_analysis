b2def83f58b15154fe0ffc638ebdfc1bea5963da
QuentGrd <quentin.gerard2@gmail.com>
2020-02-12 14:42:30 +0100

    Load data

--- data.py
+++ data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+from aif360.datasets import AdultDataset
+from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions import load_preproc_data_adult
+from aif360.algorithms.preprocessing.optim_preproc_helpers.distortion_functions import get_distortion_adult
+
 
 PATH_DATA = "./data/adult.data"
 DATASET_COLUMNS = ["age","workclass","fnlwgt","education","education_num",
@@ -8,8 +12,18 @@ DATASET_COLUMNS = ["age","workclass","fnlwgt","education","education_num",
                    "native_country","income"]
 
 def loadData():
-    return pd.read_csv(PATH_DATA,names=DATASET_COLUMNS,sep=", ",engine='python')
+    privileged_groups = [{'sex': 1}]
+    unprivileged_groups = [{'sex': 0}]
+    dataset_orig = load_preproc_data_adult(['sex'])
+
+    optim_options = {
+        "distortion_fun": get_distortion_adult,
+        "epsilon": 0.05,
+        "clist": [0.99, 1.99, 2.99],
+        "dlist": [.1, 0.05, 0]
+    }
+    return privileged_groups, unprivileged_groups, optim_options, dataset_orig
 
 if __name__ == '__main__':
-    data = loadData()
+    privileged_groups, unprivileged_groups, optim_options, data = loadData()
     print(data)
